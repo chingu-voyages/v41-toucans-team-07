9f048dfb7e18ee2de3fcc21952e2cb62469eba82
umesh <umesharma.us11@gmail.com>
2022-11-09 20:36:03 +0530

    adding characterlist for the character data  and added the data to the characters page

---
+++ black_pearl/src/pages/Character/CharacterList.js
@@ -0,0 +1,52 @@
+const CharacterList = [
+  {
+    name: 'Jack Sparrow',
+    img: 'https://tse4.mm.bing.net/th?id=OIP.YcAfbdnbUCx96K4AK0pM2AHaJ3&pid=Api&P=0',
+  },
+  {
+    name: 'Wil Turner',
+    img: 'https://vignette.wikia.nocookie.net/ppc/images/d/da/WillTurner.jpg/revision/latest?cb=20111012034400',
+  },
+  {
+    name: 'Elizabeth Swann',
+    img: 'http://images2.fanpop.com/images/photos/7700000/Elizabeth-Swann-elizabeth-swann-7790602-600-400.jpg',
+  },
+  {
+    name: 'Marty',
+    img: 'https://tse1.mm.bing.net/th?id=OIP.hniJO6EPYHgZpW8hpMPsUQAAAA&pid=Api&P=0',
+  },
+  {
+    name: 'Joshamee Gibbs',
+    img: 'https://tse3.mm.bing.net/th?id=OIP.gBDPm2D2seysMi1OXdQTIQHaFj&pid=Api&P=0',
+  },
+  {
+    name: 'Cotton',
+    img: 'https://tse3.mm.bing.net/th?id=OIP.grTTpDcgF_2ylGnEC7Q6vgHaDV&pid=Api&P=0',
+  },
+  {
+    name: 'Barbossa',
+    img: 'https://tse1.mm.bing.net/th?id=OIP.aJkzM7M1CElnuKzTM878FAHaKU&pid=Api&P=0',
+  },
+  {
+    name: 'Jack the Monkey',
+    img: 'https://tse1.mm.bing.net/th?id=OIP.WugVd3OCRtw1j7QqktnPdQHaDt&pid=Api&P=0',
+  },
+  {
+    name: 'Pintel',
+    img: 'https://tse1.mm.bing.net/th?id=OIP.ij-zhYHO5hMhkKFIg9rqlAHaE7&pid=Api&P=0',
+  },
+  {
+    name: 'Ragetti',
+    img: 'https://tse3.mm.bing.net/th?id=OIP.gRptQFVQyJZZ91DLlBURCgHaEK&pid=Api&P=0',
+  },
+  {
+    name: 'James Norrington',
+    img: 'https://i.pinimg.com/originals/b6/07/c3/b607c3e5ed11b0d9f7bce799fb034e41.jpg',
+  },
+  {
+    name: 'Governor Weatherby Swann',
+    img: 'https://vignette1.wikia.nocookie.net/disney/images/2/28/Weatherby_Swann_Younger.jpg/revision/latest?cb=20120706045259',
+  },
+];
+
+export default CharacterList;
